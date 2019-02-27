8a689a81f4a6747d8c80434ff2736f1f68740aa9
Antoine Canaguier-Durand <antoine.canaguier@gmail.com>
2019-02-27 16:20:41 +0100

    add function that generates S from D

--- collective_Smatrix.py
+++ collective_Smatrix.py
@@ -1,7 +1,21 @@
 import numpy as np
 
-def matrix_Ddip(pos,alpha,lmax):
-    '''generates the scattering matrix D
+def matrix_Sdip(pos, alpha, lmax):
+    '''generates the scattering matrix S
+    from the diffusion matrix D
+    in the case of dipolar scatterers
+    with positions pos and polarizabilities alpha
+    pos has size (3,N) and alpha has size (1,N)
+    alpha must be already multiplied by k^3 to be adimensional
+    '''
+    N = pos.shape[1]
+    D = matrix_Ddip(pos, alpha, lmax)
+    S = np.eye(3*N) + 2*D
+    return S
+
+
+def matrix_Ddip(pos, alpha, lmax):
+    '''generates the diffusion matrix D
     in the case of dipolar scatterers
     with positions pos and polarizabilities alpha
     pos has size (3,N) and alpha has size (1,N)
