161ae837600800004c4abd5f18f9c2170dd38d10
Antoine Canaguier-Durand <antoine.canaguier@gmail.com>
2019-08-15 09:26:15 +0200

    add function for TQdip matrix

--- collective_Smatrix.py
+++ collective_Smatrix.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 def matrix_Sdip(pos, alpha, lmax):
-    '''generates the scattering matrix S
+    '''
+    generates the scattering matrix S
     from the diffusion matrix D
     in the case of dipolar scatterers
     with positions pos and polarizabilities alpha
-    pos has size (3,N) and alpha has size (1,N)
+    pos has size (3,N) and alpha has size N
     alpha must be already multiplied by k^3 to be adimensional
     '''
     N = pos.shape[1] # number of particles
@@ -15,25 +16,27 @@ def matrix_Sdip(pos, alpha, lmax):
 
 
 def matrix_Ddip(pos, alpha, lmax):
-    '''generates the diffusion matrix D
+    '''
+    generates the diffusion matrix D
     in the case of dipolar scatterers
     with positions pos and polarizabilities alpha
-    pos has size (3,N) and alpha has size (1,N)
+    pos has size (3,N) and alpha has size N
     alpha must be already multiplied by k^3 to be adimensional
     '''
-
     N = pos.shape[1] # number of particles
-    TQ = matrix_TQdip(pos,alpha,lmax) # size Nsph by 3N
-    X = matrix_Xdip(pos,alpha) # size 3N by 3N
-    FT = matrix_FTdip(pos,lmax) # size 3N by Nsph
+    TQ = matrix_TQdip(pos, alpha, lmax) # size Nsph by 3N
+    X = matrix_Xdip(pos, alpha) # size 3N by 3N
+    FT = matrix_FTdip(pos, lmax) # size 3N by Nsph
     D = np.dot(TQ, np.linalg.inv(np.eye(3*N) - X)).dot(FT)
     return D # size Nsph by Nsph
 
+
 def matrix_Xdip(pos, alpha):
-    '''generates the structure X matrix
+    '''
+    generates the structure X matrix
     in the case of dipolar scatterers
     with positions pos and polarizabilities alpha
-    pos has size (3,N) and alpha has size (1,N)
+    pos has size (3,N) and alpha has size N
     alpha is multiplied by k^3 to be adimensional
     '''
 
@@ -45,22 +48,60 @@ def matrix_Xdip(pos, alpha):
                 X[3*i:3*(i+1),3*j:3*(j+1)] = alpha[j]*eval_green(pos[:,i],pos[:,j])
     return X
 
-def matrix_TQdip(pos, alpha, lmax):
-    '''generates the Q matrix in the case of dipolar scatterers
+
+
+def matrix_FTdip(pos, alpha, lmax):
+	'''
+    generates the (3N, Nsph) F*T block matrix
+    in the case of dipolar scatterers
     with positions pos and polarizabilities alpha
-    pos has size (3,N) and alpha has size (1,N)
+    pos has size (3,N) and alpha has size N
+    alpha is multiplied by k^3 to be adimensional
+    '''
+
+	N = pos.shape[1] # number of particles
+	Nsph = 2*lmax*(2*lmax+1) # number os spherical modes (with reduced notation)
+
+	# construction of the generic F matrix, size 3 by Nsph 
+	F = np.zeros((3, Nsph))
+	F[:, lmax:lmax+3] = [[1j,   0, -1j]
+						 [0,    0, sqrt(2)*j]
+						 [-1j, 1, 0]] / np.sqrt(12*np.pi)
+
+	FT = np.zeros((3*n,N))
+	for i in range(N):
+    	T = trans_j2j_reduced(-pos[:,i],lmax) # size Nsph by Nsph
+    	FT[i*3:(i+1)*3,:] = F*T # block of size 3 by Nsph
+
+    return FT # size 3N by Nsph
+
+
+
+def matrix_TQdip(pos, alpha, lmax):
+    '''
+    generates the (Nsph, 3N) T*Q block matrix
+    in the case of dipolar scatterers
+    with positions pos and polarizabilities alpha
+    pos has size (3,N) and alpha has size N
     alpha is multiplied by k^3 to be adimensional
     '''
 
     N = pos.shape[1] # number of particles
     Nsph = 2*lmax*(2*lmax+1) # number os spherical modes (with reduced notation)
 
-    Qi = np.zeros((Nsph,3))
-    Qi[lmax:lmax+3,:] = [[1,1j,0],
-                         [0,0,sqrt(2)],
-                         [-1,1j,0]]/np.sqrt(12*np.pi)
+    # construction of the generic Q matrix, size Nsph by 3 
+    Q = np.zeros((Nsph, 3))
+    Q[lmax:lmax+3,:] = [[1,  1j, 0],
+                        [0,  0,  sqrt(2)],
+                        [-1, 1j, 0]] / np.sqrt(12*np.pi)
+    
     TQ = np.zeros((Nsph,3*N))
-    for i in range(N)
+    for i in range(N):
         T = trans_h2h_reduced(pos[:,i],lmax) # size Nsph by Nsph
-        TQ[:,i*3:(i+1)*3] = alpha[i]*T*Qi # size Nsph by 3
-    return TQ
+        TQ[:,i*3:(i+1)*3] = alpha[i]*T*Q # size Nsph by 3
+
+    return TQ # size Nsph by 3N
+
+
+
+
