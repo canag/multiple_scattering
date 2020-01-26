a0a8f37b2c89fe620ec60c1c6e0a2dec6f0ec850
Antoine Canaguier-Durand <antoine.canaguier@gmail.com>
2020-01-26 14:34:34 +0100

    write all tests for CGcoeff

--- test_functions.py
+++ test_functions.py
@@ -1,10 +1,46 @@
 import pytest 
 import numpy as np
-import np.linalg.norm as norm2
+from numpy.linalg import norm
 import collective_Smatrix as tools
 import platonic_solid as positions
+
+# test on CG coefficients
+# -----------------------
+
+# literal expressions taken from wikipedia at
+# http://en.wikipedia.org/wiki/Table_of_Clebsch–Gordan_coefficients
 
 def test_CGcoeff1010():
     a = np.array([-1/np.sqrt(3), 0, np.sqrt(2/3), 0])
     b = tools.CG_coeff(1, 0, 1, 0)
-    assert norm2(a-b)/norm2(a)<1e-10
+    assert (norm(a-b)/norm(a))<1e-10
+
+def test_CGcoeff111m1():
+    a = np.array([np.sqrt(1/3), np.sqrt(1/2), np.sqrt(1/6), 0])
+    b = tools.CG_coeff(1, 1, 1, -1)
+    assert (norm(a-b)/norm(a))<1e-10
+
+def test_CGcoeff1m111():
+    a = np.array([np.sqrt(1/3), -np.sqrt(1/2), np.sqrt(1/6), 0])
+    b = tools.CG_coeff(1, -1, 1, 1)
+    assert (norm(a-b)/norm(a))<1e-10
+
+def test_CGcoeff2010():
+    a = np.array([0, -np.sqrt(2/5), 0, np.sqrt(3/5), 0])
+    b = tools.CG_coeff(2, 0, 1, 0)
+    assert (norm(a-b)/norm(a))<1e-10
+
+def test_CGcoeff2110():
+    a = np.array([0, 0, -np.sqrt(1/3), np.sqrt(2/3), 0])
+    b = tools.CG_coeff(2, 1, 1, 0)
+    assert (norm(a-b)/norm(a))<1e-10
+
+def test_CGcoeff2210():
+    a = np.array([0, 0, np.sqrt(2/3), np.sqrt(1/3), 0])
+    b = tools.CG_coeff(2, 2, 1, 0)
+    assert (norm(a-b)/norm(a))<1e-10
+
+def test_CGcoeff2020():
+    a = np.array([np.sqrt(1/5), 0, -np.sqrt(2/7), 0, np.sqrt(18/35), 0])
+    b = tools.CG_coeff(2, 0, 2, 0)
+    assert (norm(a-b)/norm(a))<1e-10
